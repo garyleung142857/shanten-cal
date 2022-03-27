b2359d1c1ccbb1ca383f4d1630d0fd0a83cf3b1c
garyleung142857 <garyleung142857@gmail.com>
2022-03-27 08:50:06 +0800

    handle agari hand

--- src/components/templates/Result.jsx
+++ src/components/templates/Result.jsx
@@ -6,26 +6,29 @@ import ResultEntry from '../elements/ResultEntry'
 
 export default Result = ({ navigation, route}) => {
   let result = route.params.result
+  let agari = false
   if (result['tiles']){
     result = result['tiles']
   } else {
+    agari = result['shanten'] === -1  // already a winning hand
     result = [{tile: null, analysis: result}]
   }
-
-  // console.log(result)
+  
   return <>
     <View style={styles.hand}>
       <Hand tiles={route.params.query} />
     </View>
-    <ScrollView>
+    {!agari && <ScrollView>
       {result.map((r, idx) => {
         return <ResultEntry
           tile={r.tile}
           analysis={r.analysis}
           key={idx}
-        />
-      })}
-    </ScrollView>
+        />})}
+    </ScrollView>}
+    {agari && <Text style={styles.agariText}>
+      和牌
+    </Text>}
   </> 
 }
 
@@ -33,4 +36,10 @@ const styles = StyleSheet.create({
   hand: {
     
   },
+  agariText: {
+    fontSize: 36,
+    padding: 10,
+    fontWeight: "700",
+    color: 'red'
+  }
 })
